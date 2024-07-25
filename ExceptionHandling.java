1970fa7c288133147081d72cea3621ebd9261929
Mearaj Sadhikunnisa Mohammad <116538474+mearaj0409@users.noreply.github.com>
2024-07-25 14:16:57 +0530

    Add files via upload

---
+++ ExceptionHandling.java
@@ -0,0 +1,26 @@
+package exceptions;
+
+public class ExceptionHandling {
+
+	public static void main(String[] args) {
+		int a=9;
+		int b=0;
+		try {
+			int c;
+			c=a/b;
+		}
+		catch(ArithmeticException e) {
+			
+			Throwable t = new Throwable(e.getMessage());
+			//t.initCause(t);
+			//e.printStackTrace();
+			//System.out.println(t.getCause());
+			System.out.println(e.getLocalizedMessage());
+			System.out.println(e.getMessage());
+			
+		}
+		
+
+	}
+
+}
